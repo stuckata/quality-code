e75f9a1a381511ae27bf4c6787cb18388efec8a1
stuckata <stu.kirkov@gmail.com>
2015-06-03 11:53:52 +0300

    start refactoring project

---
+++ Vehicle Park System/vp_himineu/vp_himineu/VehicleParkSystemProgram.cs
@@ -0,0 +1,16 @@
+﻿namespace VehicleParkSystem
+{
+    using System;
+    using System.Globalization;
+    using System.Threading;
+    using VehicleParkSystem2;
+    static class VehicleParkSystemProgram
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            var engine = new Mecanismo();
+            engine.Runrunrunrunrun();
+        }
+    }
+}
